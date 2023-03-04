ef4718478e9f7451ede9dc21c10bc3b938697b9e
woojinKim <wjinkim21@naver.com>
2023-03-04 15:03:40 +0900

    todo 내용 추가, 삭제까지 완료

--- src/App.svelte
+++ src/App.svelte
@@ -26,20 +26,45 @@
 			content: '네 번째 할일',
 			done: false
 		}
-	]
+	];
+
+	let todoValue = "";
 
 	function handleCheckTodo(id){
 		todos = todos.map(todo => {
-			if(todo.id===id){
+			if(todo.id === id){
 				todo.done = !todo.done;
 			}
-			return todo;
+			return todo
 		})
-	}
+	};
+
+	function addTodoItem(){
+		if(todoValue){
+			const newTodo = {
+				id: uuid(),
+				content: todoValue,
+				done: false,
+			}
+			todos = [...todos, newTodo];
+			todoValue = "";
+		}
+	};
+
+	function handleTodoInputKeyup(e){
+		if(e.keyCode === 13){
+			console.log(`todoValue: ${e.target.value}`)
+			addTodoItem();
+		}
+	};
+
+	function handleRemoveTodo(id){
+		todos = todos.filter(todo => todo.id !== id);
+	};
 </script>
 
 <div class="app">
-	<TodoHeader/>
+	<TodoHeader bind:todoValue={todoValue} {handleTodoInputKeyup}/>
 	<TodoInfo/>
-	<TodoList {todos}/>
+	<TodoList {todos} {handleCheckTodo} {handleRemoveTodo}/>
 </div>
--- src/components/TodoHeader.svelte
+++ src/components/TodoHeader.svelte
@@ -1,6 +1,17 @@
+<script>
+    export let todoValue;
+    export let handleTodoInputKeyup
+    /**
+     * on:keyup => 키보드의 키가 눌러졌다 올라갈 때 작동하는 이벤트
+    */
+</script>
 <header>
     <div class="wrap">
         <h1>SVELTE TODO</h1>
-        <input type="text"/>
+        <input 
+            type="text"
+            bind:value={todoValue}
+            on:keyup={handleTodoInputKeyup}
+        />
     </div>
 </header>
